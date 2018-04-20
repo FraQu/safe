f1d5b43290e676e1ca95ed1285ed06bcebb2b0a1
FraQu <FraQu@users.noreply.github.com>
2018-04-20 12:43:12 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Safe</title>
+</head>
+<body>
+<form action="Main.php" method="post" >
+    Type PIN to unlock Safe: <br />
+    <input type="password" name="pin" >
+    <input type="submit" value="OK">
+</form>
+</body>
+</html>
